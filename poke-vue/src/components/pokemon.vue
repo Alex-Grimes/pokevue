5f176d224f85d54b60d9cc66905627ae1cba7175
Alex <AlexGrimes.se@gmail.com>
2022-11-06 14:07:26 -0500

    clearing unused components

---
+++ poke-vue/src/components/pokemon.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1 class="name">Bulbasaur</h1>
+        <img src="" />
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
